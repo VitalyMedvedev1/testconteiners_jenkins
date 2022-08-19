4c5abae4e862c4fec5683add9009bc02c6555b7d
Vitaly Medvedev <vemedvedev@fil-it.ru>
2022-08-19 14:02:53 +0300

    1

--- src/test/java/com/kirekov/spring/boot/test/example/service/person/PersonCreateServiceImplDataJpaTest.java
+++ src/test/java/com/kirekov/spring/boot/test/example/service/person/PersonCreateServiceImplDataJpaTest.java
@@ -10,8 +10,10 @@ import static org.mockito.Mockito.doThrow;
 import com.kirekov.spring.boot.test.example.entity.Person;
 import com.kirekov.spring.boot.test.example.exception.ValidationFailedException;
 import com.kirekov.spring.boot.test.example.repository.PersonRepository;
+
 import java.time.ZonedDateTime;
 import java.util.List;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,72 +28,69 @@ import org.springframework.data.domain.Example;
 @DisplayName("PersonCreateServiceImpl: tests with repository auto configuration")
 class PersonCreateServiceImplDataJpaTest {
 
-  @Autowired
-  private PersonRepository personRepository;
-  @MockBean
-  private PersonValidateService personValidateService;
-  @Autowired
-  private PersonCreateService personCreateService;
+    @Autowired
+    private PersonRepository personRepository;
+    @MockBean
+    private PersonValidateService personValidateService;
+    @Autowired
+    private PersonCreateService personCreateService;
 
-  @TestConfiguration
-  static class TestConfig {
+    @TestConfiguration
+    static class TestConfig {
 
-    @Bean
-    public PersonCreateService personCreateService(
-        PersonRepository personRepository,
-        PersonValidateService personValidateService
-    ) {
-      return new PersonCreateServiceImpl(
-          personValidateService, personRepository
-      );
+        @Bean
+        public PersonCreateService personCreateService(PersonRepository personRepository,
+                                                       PersonValidateService personValidateService) {
+
+            return new PersonCreateServiceImpl(personValidateService, personRepository);
+        }
     }
-  }
 
-  @Test
-  @DisplayName("Should create one person")
-  void shouldCreateOnePerson() {
-    final var people = personCreateService.createFamily(
-        List.of("Simon"),
-        "Kirekov"
-    );
-    assertEquals(1, people.size());
-    final var person = people.get(0);
-    assertEquals("Simon", person.getFirstName());
-    assertEquals("Kirekov", person.getLastName());
-    assertTrue(person.getDateCreated().isBefore(ZonedDateTime.now()));
-  }
+    @Test
+    @DisplayName("Should create one person")
+    void shouldCreateOnePerson() {
+        final var people = personCreateService.createFamily(
+                List.of("Simon"),
+                "Kirekov"
+        );
+        assertEquals(1, people.size());
+        final var person = people.get(0);
+        assertEquals("Simon", person.getFirstName());
+        assertEquals("Kirekov", person.getLastName());
+        assertTrue(person.getDateCreated().isBefore(ZonedDateTime.now()));
+    }
 
-  @Test
-  @Disabled("This test always fails due to the fact that test itself is also transactional")
-  @DisplayName("Should rollback if any user is not validated")
-  void shouldRollbackIfAnyUserIsNotValidated() {
-    doThrow(new ValidationFailedException(""))
-        .when(personValidateService)
-        .checkUserCreation("John", "Brown");
-    assertThrows(ValidationFailedException.class, () -> personCreateService.createFamily(
-        List.of("Matilda", "Vasya", "John"),
-        "Brown"
-    ));
-    assertEquals(0, personRepository.count());
-  }
+    @Test
+    @Disabled("This test always fails due to the fact that test itself is also transactional")
+    @DisplayName("Should rollback if any user is not validated")
+    void shouldRollbackIfAnyUserIsNotValidated() {
+        doThrow(new ValidationFailedException(""))
+                .when(personValidateService)
+                .checkUserCreation("John", "Brown");
+        assertThrows(ValidationFailedException.class, () -> personCreateService.createFamily(
+                List.of("Matilda", "Vasya", "John"),
+                "Brown"
+        ));
+        assertEquals(0, personRepository.count());
+    }
 
-  @Test
-  @DisplayName("Should rollback if the only user is not validated")
-  void shouldRollbackIfOneUserIsNotValidated() {
-    doAnswer(invocation -> {
-      final String lastName = invocation.getArgument(1);
-      final var exists = personRepository.exists(Example.of(new Person().setLastName(lastName)));
-      System.out.println("Person with " + lastName + " exists: " + exists);
-      if (exists) {
-        throw new ValidationFailedException("Person with " + lastName + " already exists");
-      }
-      return null;
-    }).when(personValidateService).checkUserCreation(any(), any());
-    personRepository.saveAndFlush(new Person().setFirstName("Alice").setLastName("Purple"));
-    assertThrows(ValidationFailedException.class, () -> personCreateService.createFamily(
-        List.of("Matilda"),
-        "Purple"
-    ));
-    assertEquals(1, personRepository.count());
-  }
+    @Test
+    @DisplayName("Should rollback if the only user is not validated")
+    void shouldRollbackIfOneUserIsNotValidated() {
+        doAnswer(invocation -> {
+            final String lastName = invocation.getArgument(1);
+            final var exists = personRepository.exists(Example.of(new Person().setLastName(lastName)));
+            System.out.println("Person with " + lastName + " exists: " + exists);
+            if (exists) {
+                throw new ValidationFailedException("Person with " + lastName + " already exists");
+            }
+            return null;
+        }).when(personValidateService).checkUserCreation(any(), any());
+        personRepository.saveAndFlush(new Person().setFirstName("Alice").setLastName("Purple"));
+        assertThrows(ValidationFailedException.class, () -> personCreateService.createFamily(
+                List.of("Matilda"),
+                "Purple"
+        ));
+        assertEquals(1, personRepository.count());
+    }
 }
